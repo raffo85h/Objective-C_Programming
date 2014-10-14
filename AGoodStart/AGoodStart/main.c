b6a8a8447e8e650c394f2189dd92e7faace2f430
raffo85h <raff.rain@yahoo.it>
2014-10-14 11:58:34 +0200

    Chapter 2

---
+++ AGoodStart/AGoodStart/main.c
@@ -0,0 +1,22 @@
+//
+//  main.c
+//  AGoodStart
+//
+//  Created by Raffaele Rainone on 10/10/14.
+//  Copyright (c) 2014 Raffaele Rainone. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+    printf("Erano i tempi migliori.\n");
+    printf("Erano i tempi peggiori.\n");
+    
+    return 0;
+    //questo e' un commento
+    /* Pure questo lo e'
+     ma va avanti per
+     diverse linee
+     */
+}
