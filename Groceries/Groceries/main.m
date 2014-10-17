970217ccc9d808fd72ddb97005435f16c58019d8
raffo85h <raff.rain@yahoo.it>
2014-10-17 18:13:56 +0200

    Chapter 17, challenge

--- Groceries/Groceries/main.m
+++ Groceries/Groceries/main.m
@@ -10,8 +10,20 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        //CHALLENGE chapter 17, challenge 1: make a groceries list and thn prin each item. EASY!!!!
+        //First we create an empty list
+        NSMutableArray *groceriesList = [NSMutableArray array];
+        //Now we add items to the list:
+        [groceriesList addObject: @"pizza (pepperoni or calabrese)"];
+        [groceriesList addObject: @"potatoes, 4x"];
+        [groceriesList addObject: @"cherry vine tomatoes, 500gr"];
+        [groceriesList addObject: @"bread, 1kg"];
+        [groceriesList addObject: @"milk, 2pints"];
+        [groceriesList addObject: @"salted butter"];
+        //Now we print each item:
+        for (NSString *item in groceriesList){
+            NSLog(@"You need to buy: %@", item);
+        }
     }
     return 0;
 }
