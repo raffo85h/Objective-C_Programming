7592528d83d7e516ff1beb02544b2140f5b1d70d
raffo85h <raff.rain@yahoo.it>
2014-10-18 11:54:18 +0200

    Chapter 18 & 19, challenge

--- Stocks/BNRStockHolding.m
+++ Stocks/BNRStockHolding.m
@@ -9,5 +9,55 @@
 #import "BNRStockHolding.h"
 
 @implementation BNRStockHolding
+/*
+ //Thanks to the property declaration we can avoid the following:
+-(float)purchaseSharePrice
+{
+    return _purchaseSharePrice;
+}
+
+-(void)setPurchaseSharePrice:(float)p
+{
+    _purchaseSharePrice = p;
+}
+
+-(float)currentSharePrice
+{
+    return _currentSharePrice;
+}
+
+-(void)setCurrentSharePrice:(float)c
+{
+    _currentSharePrice = c;
+}
+
+-(int)numberOfShares
+{
+    return _numberOfShares;
+}
+
+-(void)setNumberOfShares:(int)n
+{
+    _numberOfShares = n;
+}
+*/
+-(float)costInDollars
+{
+    float p = [self purchaseSharePrice];
+    int n = [self numberOfShares];
+    return p*n;
+}
+
+-(float)valueIndollars
+{
+    float c = [self currentSharePrice];
+    int n = [self numberOfShares];
+    return c*n;
+}
+
+-(void)addToArray:(NSMutableArray *)theArray
+{
+    [theArray addObject: self];
+}
 
 @end
--- Stocks/Stocks/main.m
+++ Stocks/Stocks/main.m
@@ -7,11 +7,45 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "BNRStockHolding.h"
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        BNRStockHolding *share1 = [[BNRStockHolding alloc] init];
+        BNRStockHolding *share2 = [[BNRStockHolding alloc] init];
+        BNRStockHolding *share3 = [[BNRStockHolding alloc] init];
+        
+        //define the empty array
+        NSMutableArray *myShares = [NSMutableArray array];
+        
+        //Set the shares:
+        [share1 setPurchaseSharePrice:2.30];//Notice that setPurchaseShare is not defined by me in .h or .m. But, since I use the @option, it is automatically defined by the machine.
+        [share1 setCurrentSharePrice:4.50];
+        [share1 setNumberOfShares:40];
+        [myShares addObject:share1];
+        
+        //second share
+        //Now we use the neater dotted notation to SET the instance variables:
+        share2.purchaseSharePrice = 12.19;
+        share2.currentSharePrice = 10.56;
+        share2.numberOfShares = 90;
+        [myShares addObject:share2];
+        
+        //third share
+        share3.purchaseSharePrice = 45.10;
+        share3.currentSharePrice = 49.51;
+        share3.numberOfShares = 210;
+        [myShares addObject:share3];
+        
+        //now we print the objects:
+        for (BNRStockHolding *item in myShares)
+        {
+            //In the NSLog below only for the first variable we use the get notation with []. For the others we use again the neater dotted notation.
+            NSLog(@"I bought %d shares at the price %.2f. Therefore I spent %.2f. Now each share costs: %.2f. Therefore, now, the total value is %.2f.", [item numberOfShares], item.purchaseSharePrice, item.costInDollars, item.currentSharePrice, item.valueIndollars);
+        }
+        //Notice that THING.purchaseSharePrice can mean both a GET and a SET. In other words, it can mean both:
+        //1. [THING setPurchaseSharePrice: xxx]; and ----- e.g. THING.purchaseSharePrice = 112.05
+        //2. [THING purchaseSharePrice]. -------------- e.g. float thePriceOfThisShareWhenIBoughtIt = THING.purchaseSharePrice (here the THING.purchaseSharePrice has already been given a value!).
     }
     return 0;
 }
